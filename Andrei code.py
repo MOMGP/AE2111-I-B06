82e3b46d5bf5d69367889609d90276052f392173
AndreiT <tabara04@gmail.com>
2024-09-03 17:07:38 +0200

    s

---
+++ Andrei code.py
@@ -0,0 +1,8 @@
+import numpy
+from matplotlib import pyplot as plt
+from main import *
+
+
+#start of the matching diagram
+
+approach_speed_req=1/2*rho_0/assumed_landing_mass_fraction*(assumed_aproach_speed/1.23)**2*assumed_max_CL
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import numpy
 from matplotlib import pyplot as plt
+#CONSTANTS
+lapse_rate = -0.0065 #K m^-1
+rho_0 = 1.225 #kg m^-3
+R_air = 287.05 #J kg^-1 K^-1
+T_0 = 288.15 #K
 
+
+
+#Airplane constraints
 M_p_max = 49442 #kg
 M_cr = 0.82
 h_cr = 39000 #ft
@@ -12,9 +20,14 @@ range_MTOW_full_fuel = 13983 #km
 M_p_max_full_fuel = 26308 #km
 r_ferry = 15811 #km
 
-lapse_rate = -0.0065 #K m^-1
-rho_0 = 1.225 #kg m^-3
-R_air = 287.05 #J kg^-1 K^-1
-T_0 = 288.15 #K
 
-hello =1
+
+#ASSUMPTIONS
+assumed_max_CL=2
+assumed_maximum_takeoff_mass=150000 #kg
+assumed_aproach_speed=74.163 #aproach speed
+assumed_landing_mass_fraction=0.8 #just assumed, no calculations
+
+
+
+
