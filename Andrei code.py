83e854e43ed4fed4281a1b72db402cb3dfb8a791
jeroen04 <koppejanjeroen@gmail.com>
2024-09-03 17:24:59 +0200

    Jeroen Koppejan

--- Andrei code.py
+++ Andrei code.py
@@ -6,5 +6,3 @@ from main import *
 #start of the matching diagram
 
 approach_speed_req=1/2*rho_0/assumed_landing_mass_fraction*(assumed_aproach_speed/1.23)**2*assumed_max_CL
-style = 10
-#so sad it works, now i have to code
