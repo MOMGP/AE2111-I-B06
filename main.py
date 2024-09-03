85c2da6952e9cec8277359f2e4b5b7a1ae8d77b3
MOMGP <mariomgp04@gmail.com>
2024-09-03 15:53:29 +0200

    constants

--- main.py
+++ main.py
@@ -12,3 +12,8 @@ range_MTOW_full_fuel = 13983 #km
 M_p_max_full_fuel = 26308 #km
 r_ferry = 15811 #km
 
+lapse_rate = -0.0065 #K m^-1
+rho_0 = 1.225 #kg m^-3
+R_air = 287.05 #J kg^-1 K^-1
+T_0 = 288.15 #K
+C_L=2.0
