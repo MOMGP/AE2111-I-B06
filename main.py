53efee700f0f8b92e141b8d006420ab9fd622cd0
MOMGP <mariomgp04@gmail.com>
2024-09-03 15:23:48 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy
+from matplotlib import pyplot as plt
+
+M_p_max = 49442 #kg
+M_cr = 0.82
+h_cr = 39000 #ft
+d_to = 2790 #m
+d_ld = 1856 #m 
+range_dm = 13797 #km
+payload_dm = 27669 #kg
+range_MTOW_full_fuel = 13983 #km
+M_p_max_full_fuel = 26308 #km
+r_ferry = 15811 #km
+
