77ca2db6a9e8f8be4a09ea84806b449084dc244b
jeroen04 <koppejanjeroen@gmail.com>
2024-11-19 17:46:08 +0100

    Jeroen Koppejan

--- Work package 4/get points.py
+++ Work package 4/get points.py
@@ -1,11 +1,23 @@
-import math
-
-airfoil_chords = []
-
-
-def get_airfoil();
-    for lines in airfoil.text:
+airfoil_geometry = []
+wingbox_chords = []
 
 
+def get_points(length_1, length_2, length_3):
+    airfoil_geometry = [[0.5, 1, 2],[0.51, 1.1, 1.9],[0.52, 1.2, 1.8]] #get airfoil geometry
 
+    for i in range(len(airfoil_geometry)):  #input values for length 1
+        j = airfoil_geometry[i][0]
+        if length_1 == j:
+            wingbox_chords.append(airfoil_geometry[j][1], airfoil_geometry[j][2])
 
+    for i in range(len(airfoil_geometry)):  #input values for length 2
+        j = airfoil_geometry[i][0]
+        if length_2 == j:
+            wingbox_chords.append(airfoil_geometry[j][1], airfoil_geometry[j][2])
+
+    for i in range(len(airfoil_geometry)):  # input values for length 3
+        j = airfoil_geometry[i][0]
+        if length_3 == j:
+            wingbox_chords.append(airfoil_geometry[j][1], airfoil_geometry[j][2])
+
+    return wingbox_chords()
