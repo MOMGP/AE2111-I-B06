455b1e2ffc90db0a95b6a79531ea7c8735207edf
jeroen04 <koppejanjeroen@gmail.com>
2024-11-21 15:21:43 +0100

    fuck you

--- Work package 4/get points.py
+++ Work package 4/get points.py
@@ -4,6 +4,9 @@ airfoil_chords_list = 'AE2111-I-B06-Cracked-AF\Work package 4\Airfoil Chord List
 import pandas as pd
 import numpy as np
 
+airfoil_geometry = []
+wingbox_chords = []
+
 def get_airfoil(input_file):
     # Read airfoil points into a numpy array
     airfoil_points = pd.read_csv(input_file).to_numpy()
@@ -28,7 +31,68 @@ def get_airfoil(input_file):
 
 
 
-np.save("Airfoil_geom.npy",get_airfoil(airfoil_chords_list))
+#np.save("Airfoil_geom.npy",get_airfoil(airfoil_chords_list))
 
 
+def get_points(length_1, length_2, length_3):
+    airfoil_geometry = np.load('AE2111-I-B06-Cracked-AF\\Airfoil_geom.npy') #get airfoil geometry
 
+    for i in range(len(airfoil_geometry)-1):  #input values for length 1
+        j = airfoil_geometry[i][0]
+        k = airfoil_geometry[i+1][0]
+        if j <= length_1 <= k:
+            l = (length_1 - j) / (k - j)
+            top_surface = (1 - l) * airfoil_geometry[i][1] + l * airfoil_geometry[i + 1][1]
+            bottom_surface = (1 - l) * airfoil_geometry[i][2] + l * airfoil_geometry[i + 1][2]
+            wingbox_chords.append(top_surface)
+            wingbox_chords.append(bottom_surface)
+
+        elif j <= length_2 <= k:
+            l = (length_2 - j) / (k - j)
+            top_surface = (1 - l) * airfoil_geometry[i][1] + l * airfoil_geometry[i + 1][1]
+            bottom_surface = (1 - l) * airfoil_geometry[i][2] + l * airfoil_geometry[i + 1][2]
+            wingbox_chords.append(top_surface)
+            wingbox_chords.append(bottom_surface)
+
+        elif j  <= length_3 <= k and not length_3 == -1:
+            l = (length_3 - j) / (k - j)
+            top_surface = (1 - l) * airfoil_geometry[i][1] + l * airfoil_geometry[i + 1][1]
+            bottom_surface = (1 - l) * airfoil_geometry[i][2] + l * airfoil_geometry[i + 1][2]
+            wingbox_chords.append(top_surface)
+            wingbox_chords.append(bottom_surface)
+
+    intersect = False #set intersect false
+
+    for i in range(0, len(wingbox_chords)-2):
+        y1 = wingbox_chords[i]
+        y2 = wingbox_chords[i+2]
+        if i <= 1:
+            x1 = length_1
+            x2 = length_2
+            a = (x2 - x1) / (y2 - y1)
+            b = y1 - a * x1
+            x = np.array(np.round(length_1, 2), np.round(length_2, 2), 0.01)
+            for j in range(x):
+                y = a * x[j] + b
+                yref1 = airfoil_geometry[airfoil_geometry.index(x)][1]
+                yref2 = airfoil_geometry[airfoil_geometry.index(x)][2]
+                if not yref2 < y <yref1:
+                    intersect = True
+
+
+        elif i >= 2:
+            x1 = length_2
+            x2 = length_3
+            a = (x2 - x1) / (y2 - y1)
+            b = y1 - a * x1
+            x = np.array(np.round(length_2, 2), np.round(length_3, 2), 0.01)
+            for j in range(x):
+                y = a * x[j] + b
+                yref1 = airfoil_geometry[airfoil_geometry.index(x)][1]
+                yref2 = airfoil_geometry[airfoil_geometry.index(x)][2]
+                if not yref2 < y < yref1:
+                    intersect = True
+
+    return wingbox_chords(), intersect
+
+winbox_chords, intersect  = get_points(0.2, 0.5, -1)
