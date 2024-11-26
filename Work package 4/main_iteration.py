f3fceafb25d061bab4ba14c1b63fae8150edb35c
Mario Gonzalez Piñeiro <m.gonzalezpineiro-1@student.tudelft.nl>
2024-11-26 16:09:25 +0100

    finished getting points for particular spanwise

--- Work package 4/main_iteration.py
+++ Work package 4/main_iteration.py
@@ -1,6 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
-#from get_points import get_points
+from get_points import get_points
 from geometry_WP4_2 import centroid, moments_of_inertia
 
 
@@ -13,12 +13,18 @@ airfoil_xyy = np.load("Airfoil_geom.npy")
 possible_t = np.array([3.665, 3.264, 2.906, 2.588, 2.305, 2.053, 1.628, 1.291, 1.024, .812, .644, .511, .405, .312]) # in mm
 possible_x = airfoil_xyy[:,0]
 
+x_y_y = np.array(get_points(0.2, 0.6, 0.8, 1))
+x_pts = np.array([x_y_y[:, 0], x_y_y[:, 0]]).flatten()
+y_pts = np.array([x_y_y[:, 1], x_y_y[:, 2]]).flatten()
+plt.scatter(x_pts, y_pts)
+plt.show()
+
 def check_reqs():
     # Bending
     
 
     # Torsion
-    
+
 
     return 0
 
@@ -27,3 +33,4 @@ plt.plot(airfoil_xyy[:,0], airfoil_xyy[:,2])
 plt.show()
 
 # ADD WING TIP DEFLECTION AND TWIST 
+# Calculate sweep at particular position
