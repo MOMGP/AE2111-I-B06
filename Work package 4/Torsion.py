0b3ef0f60739509d72a8c022fc2543594c460644
Sander-with-an-e <s.t.lammers@student.tudelft.nl>
2024-11-26 17:28:50 +0100

    parameters

--- Work package 4/Torsion.py
+++ Work package 4/Torsion.py
@@ -1,4 +1,6 @@
 import math
+import numpy as np
+#from geometry_WP4_2 import centroid
 
 Weight_engine = 9630*9.81 #N
 Thrust_per_engine = 467060 #N
@@ -6,5 +8,13 @@ Lambda_LE = 0.54 #rad
 C_r = 7.63 #m
 b = 53.57 #m
 taper = 0.3
+Lambda_c2 = math.atan(math.tan(Lambda_LE) - 0.5*((2*C_r)/b)*(1-taper)) #rad
+Thrust_per_engine_perpendicular = Thrust_per_engine * math.cos(Lambda_c2) #N
+x_thrust = b/2 * 0.35
+x_engine_weight = b/2 * 0.35
 
-Lambda_c2 = math.atan(math.tan(Lambda_LE) - 0.5*((2*C_r)/b)*(1-taper))
+
+
+
+
+
