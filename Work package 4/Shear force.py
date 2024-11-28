d48ef77b0c28b1e242d2a1585cdd49b6ba71aa79
nathan-vs <nathan.vansteenberge@gmail.com>
2024-11-28 17:19:29 +0100

    new shear

--- Work package 4/Shear force.py
+++ Work package 4/Shear force.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
 from scipy.integrate import quad
-import sympy as sp
+import sympy as si
 from sympy import Symbol, integrate
+from Aero_loading_XFLR5 import Lift_for_integrating
 from Aero_loading_XFLR5 import Lift_distribution_for_any_load_case
 #extra stuff
 #x - y-direction along the wingspan
@@ -31,12 +32,14 @@ def half_span(b):
 #def integrand(x):
  #   return Lift_distribution_for_any_load_case
 
+'''
 #integrating the loading distribution
 x = Symbol('x')
 def shear_integration(t):
     return integrate(t, x)
+    '''
 #-------------********-------------*********------------*********-------------********-------------*********------------*********#
-
+'''
 def step(y):          # Shear force step function            
     if y_position<0 or y_position>half_span(b):
         S_engine = 0
@@ -52,8 +55,16 @@ y_position = np.arange(0, half_span(b), 1).tolist()
 x = sp.Symbol('x')
 #Lift_distribution_for_any_load_case(CL_d,rho,V)
 shearequation = shear_integration(2*x)
-
-for i in range(len(y_position)):
+'''
+'''for i in range(len(y_position)):
+    if y_position[i]<0 or y_position[i]>half_span(b):
+       
+    elif y_position[i]<y_engine:
+        
+    else:
+        internal_shear_force.append(shearequation.subs(x, var_sub)-step(y_position))
+'''
+"""for i in range(len(y_position)):
     var_sub = i
     if y_position[i]<0 or y_position[i]>half_span(b):
        internal_shear_force.append(shearequation.subs(x, var_sub))
@@ -61,14 +72,25 @@ for i in range(len(y_position)):
         internal_shear_force.append(shearequation.subs(x, var_sub))
     else:
         internal_shear_force.append(shearequation.subs(x, var_sub)-step(y_position))
+        """
     
 
-#print(shearequation)
-plt.plot(y_position, internal_shear_force, label="Shear Force")
-plt.xlabel("Y-position")
-plt.ylabel("Shear Force")
-plt.title("Shear Force along the span")
-plt.legend()
+
+# print(Lift_for_integrating(26.785,0.7,0.31641,241.9574))
+
+lift, span_loc = Lift_distribution_for_any_load_case(0.7,0.31641,241.9574)
+shear_force = []
+L_error_list = []
+
+
+for i in np.arange(0,26.786,0.001):
+    shear_result,L_error_result=sp.integrate.quad(Lift_for_integrating,0,i,args=(0.7,0.31641,241.9574))
+    shear_force.append(shear_result)
+    L_error_list.append(L_error_result)
+
+plt.plot(span_loc, shear_force, label="Shear Force")
+plt.xlabel("spanwise location")
+plt.ylabel("Shear force")
+plt.title("shear force dist.")
+plt.gca().set_aspect(1/4000, adjustable='box')
 plt.show()
-
-
