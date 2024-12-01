11300b79ab51ae0fce0eb53b1ada6ef9019d454e
hskrobanek <hanna.skrobanek@gmail.com>
2024-12-01 16:39:47 +0100

    bending update (adjusted limit + load factor)

--- Work package 4/bending.py
+++ Work package 4/bending.py
@@ -10,7 +10,6 @@ from Aero_loading_XFLR5 import normal_force_for_integrating
 from Aero_loading_XFLR5 import Lift_distribution_for_any_load_case
 
 #--------------------------------Initial values--------------------------------#
-
 n = 1
 Pe = 9.81*9630*n        # Engine weight [N]
 ye = 9.37             # Engine spanwise location [m]
@@ -31,16 +30,16 @@ fout=open("bending.txt", "w")                                                   
 
 def moment_inner_int(i):
     return quad(lambda x: normal_force_for_integrating(x, 0.7, 0.31641, 241.9574, 1),
-        i, 26.785,limit=50, epsabs=100)[0]
+        i, 26.785,limit=500, epsabs=100)[0]
 
 
 for i in np.arange(0,26.78,0.01):
-    moment_result, error = quad(lambda y: moment_inner_int(y),i,26.78,limit=50, epsabs=100) + n*Pe*i + Me
+    moment_result, error = quad(lambda y: moment_inner_int(y),i,26.78,limit=500, epsabs=100) + Pe*i + Me
     
     # n*Pe*i comes from a single integral of the engine weight (Pe) over the distance i. Me is the engine bending moment (no integral). n is the load factor 
 
     if i <= 9.37:                                           # Not including the engine contribution
-        moment_result = moment_result - n*Pe*i - Me
+        moment_result = moment_result - Pe*i - Me
 
     moment.append(moment_result)
     fout.write(str(moment_result))
