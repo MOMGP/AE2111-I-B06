daa90dfd0a3321300a46cdfb9e438ef7f62a3aef
Sander-with-an-e <s.t.lammers@student.tudelft.nl>
2024-12-02 20:42:48 +0100

    Added quarter chord torque function

--- Work package 4/Torsion.py
+++ Work package 4/Torsion.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
 from scipy import integrate
-from Aero_loading_XFLR5 import Lift_for_integrating, lift_dist_spanwise, normal_force_for_integrating, Lift_distribution_for_any_load_case
+from Aero_loading_XFLR5 import Lift_for_integrating, lift_dist_spanwise, normal_force_for_integrating, Lift_distribution_for_any_load_case, pitching_moment_distribution_any_CL, chord_length_interpolation
 #from geometry_WP4_2 import centroid
 
 CL_d = 3
@@ -69,7 +69,15 @@ torque_engine_thrust = Thrust_per_engine_perpendicular * 2.085 #based on technic
 torque_engine_weight = weight_engine * ((C_r*(1-((1-taper)*((b/2 * 0.35)/(b/2)))))/2 + 3.5) #based on technical drawing I am assuming that the weight of the engine acts at 3.5 meter in front of LE, centroid is assumed at c/2
 total_normal_torque, err_normal_torque = sp.integrate.quad(lambda x, CL_d, rho, V, n: normal_force_for_integrating(x, CL_d, rho, V, n) * moment_arm_normal_spanwise(x), 0,26.78, args=(0.7,0.31641,241.9574,1), limit=50, epsabs=100)
 
-total_torque = total_normal_torque + torque_engine_thrust - torque_engine_weight
+def internal_quarter_chord_torque_spanwise(y, CL_D, rho, V):
+    total_quarter_pitching_moment_coefficient, err_total_quarter_pitching_moment_coefficient = sp.integrate.quad(pitching_moment_distribution_any_CL,0,y,args=(CL_d),limit=50, epsabs=100)
+    chord_at_y = chord_length_interpolation(y)
+    q = 0.5 * rho * V**2
+    total_quarter_chord_pitching_moment = total_quarter_pitching_moment_coefficient * q * chord_at_y
+    return total_quarter_chord_pitching_moment
+
+
+total_torque = total_normal_torque + internal_quarter_chord_torque_spanwise(26.78, 0.7,0.31641,241.9574) + torque_engine_thrust - torque_engine_weight
 
 torque_list = []
 torque_error_list = []
@@ -77,6 +85,7 @@ torque_error_list = []
 fout=open("torque.txt", "w")
 for i in np.arange(0,26.78,0.01):
     torque_result,torque_error_result=sp.integrate.quad(lambda x,CL_d,rho,V,n: normal_force_for_integrating(x,CL_d,rho,V,n) * moment_arm_normal_spanwise(x),0,i,args=(0.7,0.31641,241.9574,1),limit=50, epsabs=100)
+    torque_result += internal_quarter_chord_torque_spanwise(i, 0.7,0.31641,241.9574)
     if i >= 9.37:
         torque_result = torque_result + torque_engine_thrust - torque_engine_weight
     torque_result=(-total_torque+torque_result) #Nm
@@ -101,7 +110,7 @@ def internal_torque_at_x(x, CL_d, rho, V, n):
     torque_list = []
     torque_error_list = []
     total_normal_torque, err_normal_torque = sp.integrate.quad(lambda x, CL_d, rho, V, n: normal_force_for_integrating(x, CL_d, rho, V, n) * moment_arm_normal_spanwise(x), 0,26.78, args=(CL_d, rho, V, n), limit=50, epsabs=100)
-    total_torque = n * total_normal_torque + torque_engine_thrust - torque_engine_weight
+    total_torque = n * total_normal_torque + internal_quarter_chord_torque_spanwise(26.78, CL_d, rho, V) + torque_engine_thrust - torque_engine_weight
     if x == 0:
         return -total_torque
     if x > b/2:
@@ -111,6 +120,7 @@ def internal_torque_at_x(x, CL_d, rho, V, n):
         if x > b/2:
             break
         torque_result, torque_error_result = sp.integrate.quad(lambda x, CL_d, rho, V, n: normal_force_for_integrating(x, CL_d, rho, V, n) * moment_arm_normal_spanwise(x), 0, i, args=(CL_d, rho, V, n), limit=50, epsabs=100)
+        torque_result = torque_result + internal_quarter_chord_torque_spanwise(i, CL_d, rho, V)
         if i >= 9.37:
             torque_result = torque_result + torque_engine_thrust - torque_engine_weight
         torque_result = (-total_torque + torque_result) #Nm
@@ -118,64 +128,6 @@ def internal_torque_at_x(x, CL_d, rho, V, n):
         torque_error_list.append(torque_error_result)
     return torque_list[-1]
 
+print(internal_torque_at_x(26.78, 0.7, 0.31641, 241.9574,1))
 
 
-# -----------------------------------------------------------------------------------------------------------------------------
-# -----------------------------------------------------------------------------------------------------------------------------
-
-
-
-'''
-# ACTING TORQUES AROUND SHEAR CENTER (NOT INTERNAL TORQUE) ---------------------------------------------------------
-#Creating a list of the moment arm of the lift with respect to the assumed shear center position
-moment_arm_lift = chord_at_span_loc/4 #assuming lift at c/4 of unswept and centroid at c/2 of unswept
-
-#Creating a list of the lift torque around the shear center along the span
-Lift_torque_spanwise = moment_arm_lift * lift_dist_spanwise
-
-#Add contributions of the thrust of the engine
-thrust_force = []
-moment_arm_thrust = []
-n=-1
-for i in span_loc:
-    n += 1
-    if i >= b / 2 * 0.35:
-        for s in range(0,26786):
-            if s == 9375:
-                thrust_force.append(Thrust_per_engine_perpendicular)
-                moment_arm_thrust.append(2.085) #based on technical drawing I am assuming that the thrust acts at center of engine which is assumed to be one radius of the engine from the center of the wingbox, which is 2.085 m. Centroid is assumed at c/2 on the camber line.
-            else:
-                thrust_force.append(0)
-                moment_arm_thrust.append(0)
-        break
-Thrust_torque_spanwise = np.array(thrust_force) * np.array(moment_arm_thrust)
-
-#Add contributions of the weight of the engine
-weight_force_engine = []
-moment_arm_weight_engine = []
-k=-1
-for i in span_loc:
-    k += 1
-    if i >= b / 2 * 0.35:
-        for s in range(0,26786):
-            if s == 9375:
-                weight_force_engine.append(weight_engine)
-                moment_arm_weight_engine.append((chord_at_span_loc[9375]/2) + 3.5) #based on technical drawing I am assuming that the weight of the engine acts at 3.5 meter in front of LE, centroid is assumed at c/2
-            else:
-                weight_force_engine.append(0)
-                moment_arm_weight_engine.append(0)
-        break
-Weight_engine_torque_spanwise = np.array(weight_force_engine) * np.array(moment_arm_weight_engine)
-
-#List of summation of acting torques at each spanwise location
-Torque_spanwise = Lift_torque_spanwise + Thrust_torque_spanwise - Weight_engine_torque_spanwise
-
-
-plt.plot(span_loc, Torque_spanwise)
-plt.xlabel("spanwise location")
-plt.ylabel("Torque")
-plt.xlim(0,b/2)
-plt.gca().set_aspect(1/100000, adjustable='box')
-plt.show()
-# -----------------------------------------------------------------------------------------------------------------------------
-'''
