b77e0f31a0a7ed2b367b89ead9394e2bf2293ad8
SandorKoot <koot.sandor@gmail.com>
2024-12-02 14:19:54 +0100

    nothing new added yet

--- Work package 4/main_iteration.py
+++ Work package 4/main_iteration.py
@@ -1,8 +1,9 @@
 import numpy as np
 import math
+import time
 import scipy
 from matplotlib import pyplot as plt
-from scipy.integrate import dblquad, quad
+from scipy.integrate import dblquad, quad, quad_vec
 from get_points import get_points, get_geom_from_points
 from geometry_WP4_2 import centroid, moments_of_inertia
 # from bending import bending_moment, S
@@ -30,10 +31,10 @@ for i in geom:
     x_vals.append(i[1][0])
     y_vals.append(i[0][1])
     y_vals.append(i[1][1])
-plt.plot(x_vals, y_vals)
-plt.plot(airfoil_xyy[:,0], airfoil_xyy[:,1])
-plt.plot(airfoil_xyy[:,0], airfoil_xyy[:,2])
-plt.show()
+# plt.plot(x_vals, y_vals)
+# plt.plot(airfoil_xyy[:,0], airfoil_xyy[:,1])
+# plt.plot(airfoil_xyy[:,0], airfoil_xyy[:,2])
+# plt.show()
 
 def check_reqs():
     return 0
@@ -54,41 +55,87 @@ def centroid_distance(centroid,airfoil_points):
 #     return(v) #idk if this works at all lol?
 # print(v_y(bending_moment(S),E,I_xx))
 
+
+
 # Integrate the bending moment to find the deflection w(x)
-
-def I_xx(x): # dummy ixx :3
-    return 0.1 - 0.002 * x 
-
-def load_distribution(x):
-    return 1000 * x**2
-
-def shear_force(x):
-    result, _ = quad(load_distribution, 0, x)  # Shear force is the integral of load
+start = time.time()
+def I_xx(x): # dummy ixx
+    return 0.5 - 0.015 * x 
+def load_distribution(x): #dummy load distribution
+    if x <= 10:
+        return 50000
+    else:
+        return 40000 - 10 * x ** 2
+def shear_force(x): 
+    result, _ = quad(load_distribution, 0, x, limit=500)  # Shear force is the integral of load
     return result
-def bending_moment(x): #dummy bending moment formula
-    return 800000 * x 
-def angle_of_rotation(x):
-    result, _ = quad(lambda xi: bending_moment(xi) / (E * I_xx(x)), 0, x)
+def bending_moment(x): 
+    result, _ = quad(shear_force, 0, x, limit=500)  # Bending moment is the integral of shear force
+    return result 
+def angle_of_rotation(x): # 
+    result, _ = quad(lambda xi: bending_moment(xi) / (E * I_xx(x)), 0, x, limit=500)
     return result
-def deflection(x):
+# def deflection(x):
     result, _ = quad(angle_of_rotation,0,x)
     return result
 
 # Calculate and plot results
-x_vals = np.linspace(0, b/2, 100) #create # amount of values evenly spaced
-deflection_vals = [deflection(x) for x in x_vals] 
+x_vals = np.linspace(0, b/2, 10) #create # amount of values evenly spaced
+load_vals = [load_distribution(x) for x in x_vals]
+shear_vals = [shear_force(x) for x in x_vals]
+moment_vals = [bending_moment(x) for x in x_vals]
+angle_of_rotation_vals = [angle_of_rotation(x) for x in x_vals]
+# deflection_vals = [deflection(x) for x in x_vals] 
+
+
+print('Angle of rotation is: ', angle_of_rotation(b/2), 'degrees or rad idk')
+end = time.time()
+print(end - start, "seconds")
+
 
 plt.figure(figsize=(8, 4))
-# Deflection
-#plt.subplot(1, 1, 1) bending moment can be added later aswell if needed :3
-plt.plot(x_vals, deflection_vals, label="Deflection w(x)", color="purple")
+
+# Load distribution
+plt.subplot(5, 1, 1)
+plt.plot(x_vals, load_vals, label="Load Distribution q(x)", color="blue")
+plt.ylabel("Load (N/m)")
+plt.grid(alpha=0.3)
+plt.legend()
+
+# Shear force
+plt.subplot(5, 1, 2)
+plt.plot(x_vals, shear_vals, label="Shear Force V(x)", color="green")
+plt.ylabel("Shear Force (N)")
+plt.grid(alpha=0.3)
+plt.legend()
+
+# Bending moment
+plt.subplot(5, 1, 3)
+plt.plot(x_vals, moment_vals, label="Bending Moment M(x)", color="red")
+plt.ylabel("Moment (Nm)")
+plt.grid(alpha=0.3)
+plt.legend()
+
+plt.subplot(5, 1, 4) # bending moment can be added later aswell if needed :3
+plt.plot(x_vals, angle_of_rotation_vals, label="Deflection w(x)", color="purple")
 plt.xlabel("Position along the beam (m)")
 plt.ylabel("Deflection (m)")
 plt.grid(alpha=0.3)
 plt.legend()
 plt.show()
 
-print('Angle of rotation is: ', angle_of_rotation(b/2 * 180 / math.pi), 'degrees')
+
+
+# Deflection
+#plt.subplot(1, 1, 1) bending moment can be added later aswell if needed :3
+# plt.plot(x_vals, deflection_vals, label="Deflection w(x)", color="purple")
+# plt.xlabel("Position along the beam (m)")
+# plt.ylabel("Deflection (m)")
+# plt.grid(alpha=0.3)
+# plt.legend()
+# plt.show()
+
+
 def bending_stress(bending_moment, y_max, I_xx):
     sigma = (bending_moment * y_max)/I_xx
     return(sigma) 
