eecfa508b34485efe05e462aa4cab335321712b7
SandorKoot <koot.sandor@gmail.com>
2024-12-03 15:17:19 +0100

    a

--- Work package 4/main_iteration.py
+++ Work package 4/main_iteration.py
@@ -23,7 +23,7 @@ airfoil_xyy = np.load("Airfoil_geom.npy")
 possible_t = np.array([3.665, 3.264, 2.906, 2.588, 2.305, 2.053, 1.628, 1.291, 1.024, .812, .644, .511, .405, .312]) # in mm
 possible_x = np.arange(0.2, 0.7, 0.05)
 pos_string_num = np.arange(20, 40, 4) #based on Jeroen resource
-graphs = False
+graphs = True
 x_y_y = np.array(get_points(0.2, 0.45, 0.65, 1))
 geom = get_geom_from_points(x_y_y, [0.01 for i in range(7)])
 x_vals = []
@@ -49,7 +49,7 @@ def I_xx(I_xx_root,x): #Assuming I_xx scales linearly with length, as thickness 
 
 shear = []
 bending = []
-n_points = 10 # decrease number for higher amount of points to integrate over (a.k.a. increase accuracy
+n_points = 50 # decrease number for higher amount of points to integrate over (a.k.a. increase accuracy
 with open("shear.txt", 'r') as f:
     count = 0
     for i in f:
@@ -57,13 +57,13 @@ with open("shear.txt", 'r') as f:
         count += 1
         if count % n_points == 0 or count == 1:
             shear.append(line)
-with open("bending.txt", 'r') as f:
-    count = 0
-    for i in f:
-        line = i.strip('\n')
-        count += 1
-        if count % n_points == 0 or count == 1:
-            bending.append(line)          
+# with open("bending.txt", 'r') as f:
+#     count = 0
+#     for i in f:
+#         line = i.strip('\n')
+#         count += 1
+#         if count % n_points == 0 or count == 1:
+#             bending.append(line)          
 x_vals = np.linspace(0,b/2,len(shear)) # This doesn't give completely accurate numbers but its slighty off (~1%) since i didnt find a better method
 bending_x_vals = np.arange(0,b/2,1) # taken from bending.py (assuming since only 27 (x = 0-26) values are given in bending.txt)
 shear_interp_func = scipy.interpolate.interp1d(x_vals, shear, kind='quadratic', fill_value="extrapolate") #Unsure if quadratic is appropriate
@@ -73,7 +73,7 @@ start = time.time()
 def shear_force(x): #Just defines the interpolation function for the shear force. I
     result = shear_interp_func(x)
     # result, _ = quad(load_distribution, 0, x, limit=200)  # Shear force is the integral of load
-    return result
+    return result * 1000
 def bending_moments(x): #Integrates the interpolated function of shear, and adds the interpolation functions of bending provided by bending.py
     result, _ = quad(shear_force,x,b/2)
     return result # + bending_interp_func #when i get it!!
