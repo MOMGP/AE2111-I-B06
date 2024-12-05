fff22fa30362e8e265cfd39bab1827c7a06266e4
Sander-with-an-e <s.t.lammers@student.tudelft.nl>
2024-12-05 15:05:26 +0100

    fixed torsion

--- Work package 4/Torsion.py
+++ Work package 4/Torsion.py
@@ -107,9 +107,9 @@ def internal_torque_diagram (CL_d, rho, V, n):
     plt.title("Internal Torque distribution function")
     plt.show()
 
-internal_torque_diagram(0.7,0.31641,241.9574,1)
-internal_torque_diagram(0.7,1.225,362.94,2.5)
-internal_torque_diagram(0.7,1.225,241.96,-1)
+#internal_torque_diagram(0.7,0.31641,241.9574,1)
+internal_torque_diagram(0.08428454065091404,1.225,362.94,2.5) #critical load case
+internal_torque_diagram(0.1896402164645566,1.225,241.96,-1) #critical load case
 # -------------------------------------------------------------------------------------------------------------------------
 
 
