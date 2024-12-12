109bb4a7c03cd1c3c4fd356dc7781ba93495882c
jeroen04 <koppejanjeroen@gmail.com>
2024-12-12 16:33:08 +0100

    made the new files

---
+++ Work package 5/main iteration.py
@@ -0,0 +1,8 @@
+from Geometry import get_design
+
+spar_1, spar_2, spar_3, thickness_top_bottom, thickness_sides, end_second_cell = get_design(1)
+
+skin_thickness = []
+stringers_spars = []
+stringers_cell1 = []
+stringers_cell2 = []
