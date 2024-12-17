0fa0f9a75d0ebf5dd100567c08863b58097b9223
SandorKoot <koot.sandor@gmail.com>
2024-12-17 15:18:39 +0100

    Moved kc function/graph to work package 5

--- Work package 5/Bucklings eqs.py
+++ Work package 5/Bucklings eqs.py
@@ -1,6 +1,6 @@
 import numpy as np
 import math
-from #Kc_function.py import
+from Kc_function.py import kc
 #Parameters
 k_v = -1 # ASSUMED factor of average shear compared to max 
 K = 1 # End condition factor of the column, assumed to be pinned; 1. This is because we assume the stringers to be continuous.
@@ -19,12 +19,12 @@ def crit_web_shear(t,a,b):
     crit_web_shear = math.pi() ** 2 * k_s * E / (12 * (1 - v ** 2)) * (t / b) **2
     return crit_web_shear
 
-def crit_skin_stress(t,a,b):
-    #k_c = kc(a,b)
+def crit_skin_stress(t,a,b): #Critical stress for skin buckling
+    k_c = kc(a,b)
     crit_stress = math.pi() ** 2 * k_c * E / (12 * (1 - v ** 2)) * (t / b) **2
     return crit_stress
 
-def crit_stringer_stress(K,L,I,A): #L= length, I = mmoi, A = area
+def crit_stringer_stress(K,L,I,A): #L= length, I = mmoi, A = area, stringer column buckling stress
     #I could be function dunno :3
     crit_column_stress = K * math.pi() * E * I / (L ** 2 * A)
     return crit_column_stress
