cec95901eb82bc22515df0b96340b3e84a745d47
SandorKoot <koot.sandor@gmail.com>
2024-12-17 15:36:08 +0100

    s

--- Work package 5/Bucklings eqs.py
+++ Work package 5/Bucklings eqs.py
@@ -1,10 +1,11 @@
 import numpy as np
 import math
-from Kc_function.py import kc
+from Kc_function import Kc
+
 #Parameters
 k_v = -1 # ASSUMED factor of average shear compared to max 
 K = 1 # End condition factor of the column, assumed to be pinned; 1. This is because we assume the stringers to be continuous.
-A = 
+A = -1 # area of the stringer thats set by some constraint :P
 E = 72400000000 # youngs modulus
 v = 0.33 # poisson ratio for our material
 
@@ -15,17 +16,19 @@ def shear(h1,t1,h2,t2,h3,t3,V,q1,q2):
     return tau_max
 #Critical buckling from shear for the web
 def crit_web_shear(t,a,b):
-    #k_s = ks(a,b)
-    crit_web_shear = math.pi() ** 2 * k_s * E / (12 * (1 - v ** 2)) * (t / b) **2
+    #k_s = Ks(a,b)
+    crit_web_shear = math.pi ** 2 * k_s * E / (12 * (1 - v ** 2)) * (t / b) **2
     return crit_web_shear
 
 def crit_skin_stress(t,a,b): #Critical stress for skin buckling
-    k_c = kc(a,b)
-    crit_stress = math.pi() ** 2 * k_c * E / (12 * (1 - v ** 2)) * (t / b) **2
+    k_c = Kc(a,b)
+    print(k_c)
+    crit_stress = math.pi ** 2 * k_c * E / (12 * (1 - v ** 2)) * (t / b) **2
     return crit_stress
 
 def crit_stringer_stress(K,L,I,A): #L= length, I = mmoi, A = area, stringer column buckling stress
     #I could be function dunno :3
-    crit_column_stress = K * math.pi() * E * I / (L ** 2 * A)
+    crit_column_stress = K * math.pi * E * I / (L ** 2 * A)
     return crit_column_stress
 
+print(crit_skin_stress(0.01,2,1))
