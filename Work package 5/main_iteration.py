b1f6ab5ecb56016c87998f3be8f1ff9bf89dc74b
jeroen04 <koppejanjeroen@gmail.com>
2024-12-19 15:54:46 +0100

    added correct number of stringers, number of ribs and skin thickness

--- Work package 5/main_iteration.py
+++ Work package 5/main_iteration.py
@@ -4,11 +4,16 @@ from Divide_Geom import design
 
 geometry = get_design(0)
 
-skin_thickness = np.arange(0.001,0.01,0.001)
-stringers_spars = np.arange(3,33,3)
-stringers_cell1 = np.arange(2,52,2)
-stringers_cell2 = np.arange(2,52,2)
-number_of_ribs = np.arange(3,50,1)
+#longer list if iteration doesnt take all day
+skin_thickness = [2.05, 1.8, 1.63, 1.4, 1.29, 1.1, 1.02, 0.91, 0.81, 0.71, 0.64, 0.58, 0.51, 0.46, 0.43]
+
+#shorter list if iteration does take all day
+#skin_thickness = [2.05, 1.8, 1.4, 1.1, 0.81, 0.64, 0.51, 0.43]
+
+stringers_spars = np.arange(5,10,1) #is per surface, so total is *3 per half wing
+stringers_cell1 = np.arange(9,20,1) #is per surface, so total is *2 per half wing
+stringers_cell2 = np.arange(14,30,1) #is per surface, so total is *2 per half wing
+number_of_ribs = np.arange(7,20,1) #is per half wing, so total is *2
 
 stringers = [stringers_spars[0], stringers_cell1[0], stringers_cell2[0]]
 
