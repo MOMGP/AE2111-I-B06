bf52811706aee65a4ae431c3ef4dae78708c0e14
Sander-with-an-e <s.t.lammers@student.tudelft.nl>
2024-12-19 14:49:47 +0100

    name change

--- Work package 5/Margin of Safety Tension.py
+++ Work package 5/Margin of Safety Tension.py
@@ -5,6 +5,7 @@ I_xx = []
 y_max = []
 moment = moment_at_full_position(CL_d,rho,V,n)
 
+
 for i in range(0,26.78,0.01):
     stress = (moment[i] * y_max[i]) / I_xx[i]
     spanwise_stress.append(stress)
