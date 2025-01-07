3c7297278b82dcecb2e68234dd2916db7117f6a8
Sander-with-an-e <s.t.lammers@student.tudelft.nl>
2025-01-07 15:00:29 +0100

    MOS T

--- Work package 5/Margin of Safety Tension.py
+++ Work package 5/Margin of Safety Tension.py
@@ -5,8 +5,10 @@ spanwise_stress = []
 I_xx = []
 y_max = []
 moment = moment_at_full_position(CL_d,rho,V,n)
-
+fail_stress = []
 
 for i in range(0,26.78,0.01):
     stress = (moment[i] * y_max[i]) / I_xx[i]
     spanwise_stress.append(stress)
+
+MOS = fail_stress / spanwise_stress
